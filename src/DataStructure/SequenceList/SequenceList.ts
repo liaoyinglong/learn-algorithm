46222e77c4e484b2614bf8736b8449fa79a47d9a
liaoyinglong <vigossliao@gmail.com>
2021-03-14 21:45:00 +0800

    :construction: 添加顺序表的实现

---
+++ src/DataStructure/SequenceList/SequenceList.ts
@@ -0,0 +1,38 @@
+/**
+ * 顺序表
+ */
+export class SequenceList<T> {
+  private nextIndex: number;
+
+  constructor(private list: T[] = []) {
+    this.nextIndex = this.list.length;
+  }
+
+  getList() {
+    return this.list;
+  }
+
+  push(item: T) {
+    this.list[this.nextIndex] = item;
+    this.nextIndex++;
+  }
+
+  get(i: number) {
+    if (i < 1 || i > this.list.length) {
+      return;
+    }
+    return this.list[i];
+  }
+
+  indexOf(item: T) {
+    return this.list.indexOf(item);
+  }
+
+  inset(item: T, i = 0) {
+    this.list.splice(i, 0, item);
+  }
+
+  remove(i: number) {
+    this.list.splice(i, 1);
+  }
+}
