31fc412a19ece59c1ce0d1e57c3db3e9275f788b
liaoyinglong <vigossliao@gmail.com>
2021-03-28 21:19:35 +0800

    :construction: 使用 swc jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  preset: "ts-jest",
+  // preset: "ts-jest",
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc-node/jest",
+      {
+        target: "es2019",
+        experimentalDecorators: true,
+        emitDecoratorMetadata: true,
+        keepClassNames: true,
+        sourcemap: true,
+      },
+    ],
+  },
   testEnvironment: "node",
   moduleNameMapper: {
     "^src/(.*)$": "<rootDir>/src/$1",
