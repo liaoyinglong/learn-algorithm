2d17f69bcf355665455dd9c7b70549691853ae2c
liaoyl <liaoyl@akulaku.com>
2021-07-23 17:08:26 +0800

    chore: 配置更改

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,5 @@
-let enableDebugger = (() => {
-  const { NODE_OPTIONS } = process.env;
-  // NODE_OPTIONS: "--require C:\\Users\\long8\\AppData\\Local\\Temp\\debugConnector.211.7142.14.js",
-  return !!NODE_OPTIONS && NODE_OPTIONS.includes("debug");
-})();
-
-const config = {
-  testEnvironment: "node",
-  moduleNameMapper: {
-    "^src/(.*)$": "<rootDir>/src/$1",
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "./config/esbuild-jest.js",
   },
 };
-
-if (enableDebugger) {
-  config.preset = "ts-jest";
-} else {
-  config.transform = {
-    "^.+\\.(t|j)sx?$": [
-      "@swc-node/jest",
-      {
-        target: "es2019",
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true,
-        keepClassNames: true,
-        sourcemap: true,
-      },
-    ],
-  };
-}
-
-module.exports = config;
